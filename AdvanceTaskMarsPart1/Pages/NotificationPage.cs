d86aba64b47188005525c01998cf5b0084eb42cd
SasikalaSudharsan <sasi.ei34@gmail.com>
2024-03-05 23:17:42 +0000

    Implemented Notifications tab using Nunit

---
+++ AdvanceTaskMarsPart1/Pages/NotificationPage.cs
@@ -0,0 +1,67 @@
+﻿using AdvanceTaskMarsPart1.Utilities;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvanceTaskMarsPart1.Pages
+{
+    public class NotificationPage : CommonDriver
+    {
+        private IWebElement NotificationTab   => driver.FindElement(By.XPath("//div[@class='ui top left pointing dropdown item']"));
+        private IWebElement SeeAllLink        => driver.FindElement(By.XPath("//a[text()='See All...']"));
+        private IWebElement LoadMoreButton    => driver.FindElement(By.XPath("//a[@class='ui button']"));
+        private IReadOnlyCollection<IWebElement> RequestsList => driver.FindElements(By.XPath("//div[@class='fourteen wide column']"));
+        private IWebElement ShowLessButton    => driver.FindElement(By.XPath("//a[text()='...Show Less']"));
+        private IWebElement SelectCheckbox    => driver.FindElement(By.XPath("(//div[@class='one wide column']/input[@type='checkbox'])[1]"));
+        private IWebElement DeleteButton      => driver.FindElement(By.XPath("//div[@data-tooltip='Delete selection']"));
+        private IWebElement Message           => driver.FindElement(By.XPath("//div[@class='ns-box-inner']"));
+
+        public void LoadMore_Notification()
+        {
+            Wait.WaitToBeClickable("XPath", "//div[@class='ui top left pointing dropdown item']", 4);
+            NotificationTab.Click();
+            Wait.WaitToBeClickable("XPath", "//a[text()='See All...']", 4);
+            SeeAllLink.Click();
+            Wait.WaitToBeClickable("XPath", "//a[@class='ui button']", 4);
+            LoadMoreButton.Click();
+        }
+
+        public List<IWebElement> getRequestsList()
+        {
+            Thread.Sleep(4000);
+            return new List<IWebElement>(RequestsList);
+        }
+
+        public void ShowLess_Notification()
+        {
+            Wait.WaitToBeClickable("XPath", "//div[@class='ui top left pointing dropdown item']", 4);
+            NotificationTab.Click();
+            Wait.WaitToBeClickable("XPath", "//a[text()='See All...']", 4);
+            SeeAllLink.Click();
+            Wait.WaitToBeClickable("XPath", "//a[@class='ui button']", 4);
+            LoadMoreButton.Click();
+            Wait.WaitToBeClickable("XPath", "//a[text()='...Show Less']", 4);
+            ShowLessButton.Click();
+        }
+
+        public void Delete_Notification()
+        {
+            Wait.WaitToBeClickable("XPath", "//div[@class='ui top left pointing dropdown item']", 4);
+            NotificationTab.Click();
+            Wait.WaitToBeClickable("XPath", "//a[text()='See All...']", 4);
+            SeeAllLink.Click();
+            Wait.WaitToBeClickable("XPath", "(//div[@class='one wide column']/input[@type='checkbox'])[1]", 4);            
+            SelectCheckbox.Click();            
+            DeleteButton.Click();
+        }
+
+        public string getMessage()
+        {
+            Wait.WaitToExist("XPath", "//div[@class='ns-box-inner']", 4);
+            return Message.Text;
+        }
+    }
+}
---
+++ AdvanceTaskMarsPart1/Tests/Notification_Tests.cs
@@ -0,0 +1,60 @@
+﻿using AdvanceTaskMarsPart1.Pages;
+using AdvanceTaskMarsPart1.Utilities;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace AdvanceTaskMarsPart1.Tests
+{
+    public class Notification_Tests : CommonDriver
+    {
+        LoginPage loginPageObj;
+        NotificationPage notificationPageObj;
+
+        public Notification_Tests()
+        {
+            loginPageObj = new LoginPage();
+            notificationPageObj = new NotificationPage();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            Initialize();
+            loginPageObj.LoginActions();
+        }
+
+        [Test, Order(1), Description("This test is displaying the requests list when click on LoadMore button")]
+        public void LoadMore_Notification()
+        {
+            notificationPageObj.LoadMore_Notification();
+
+            List<IWebElement> RequestsList = notificationPageObj.getRequestsList();
+            int ActualCount = RequestsList.Count;
+            Assert.That(ActualCount == 10, "After clicking 'LoadMore', number of requests is not equal to original count");
+        }
+
+        [Test, Order(2), Description("This test is shrinking the requests list when click on ShowLess button")]
+        public void ShowLess_Notification()
+        {
+            notificationPageObj.ShowLess_Notification();
+
+            List<IWebElement> RequestsList = notificationPageObj.getRequestsList();
+            int ActualCount = RequestsList.Count;
+            Assert.That(ActualCount == 5, "After clicking 'ShowLess', number of requests is not equal to original count");
+        }
+
+        [Test, Order(3), Description("This test is deleting the requests from the notifications list")]
+        public void Delete_Notification()
+        {
+            notificationPageObj.Delete_Notification();
+            string ActualMessage = notificationPageObj.getMessage();
+            Assert.That(ActualMessage == "Notification updated", "Notification has not been deleted");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Close();
+        }
+    }
+}
